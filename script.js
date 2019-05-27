462531213bb3da98e23d95bc3fd97e26998a6795
Marat Enaliev <enamarat@yandex.ru>
2019-05-27 13:37:57 +0300

    Show warning if an input field is empty.

--- script.js
+++ script.js
@@ -2,16 +2,36 @@ let tasks = [];
 
 const addTask = () => {
   const input = document.getElementById("add_task_input");
+  const list = document.querySelector(".tasks");
+  const li = document.createElement('li');
+  const existingListItems = document.querySelectorAll("li");
+  let listContent = null;
 
+  const hideWarning = () = > {
+    for (let i = 0; i < existingListItems.length; i++) {
+      if (existingListItems[i].textContent.toLowerCase() === "no data is entered!") {
+        list.removeChild(existingListItems[i]);
+      }
+    }
+  }
+
+  // if no data is entered in the input field, show a warning
   if (input.value.length === 0) {
-    console.log('No data is entered!');
+    // prevent warning duplication
+    hideWarning();
+    // create a warining
+    listContent = "<li class='warning'>";
+    listContent = "No data is entered!";
+    listContent += "</li>";
+    li.innerHTML = listContent;
+    list.appendChild(li);
   } else {
     tasks.push(input.value);
-    console.log(tasks);
-      const list = document.querySelector(".tasks");
-      const li = document.createElement('li');
-
-      let listContent = "<li>";
+  console.log(tasks);
+      // if warning about empty input was shown earlier, hide it
+      hideWarning();
+      
+      listContent = "<li>";
       listContent += tasks[tasks.length-1];
       listContent += "<input type='checkbox'> </input>";
       listContent += "</li>";
@@ -26,14 +46,19 @@ const addTask = () => {
 document.getElementById("add_task_button").addEventListener("click", addTask);
 
 
+
 const deleteTask = () => {
-  const checkboxes = document.querySelectorAll("input[type]");
+  const tasksToDelete = [];
+  const checkboxes = document.querySelectorAll("input[type='checkbox']");
   for (let i = 0; i < checkboxes.length; i++) {
     if (checkboxes[i].checked === true) {
-      tasks.splice(i, 1);
       checkboxes[i].parentNode.parentNode.removeChild(checkboxes[i].parentNode);
+      tasksToDelete.push(checkboxes[i].parentNode.textContent);
+      console.log(tasksToDelete);
     }
   }
+
+
 }
 
 document.getElementById("delete").addEventListener("click", deleteTask);
